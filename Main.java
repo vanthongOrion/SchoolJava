049892306ffe081af2289fc037fe60ad22192f04
IT21029 <vanthongvision977@gmail.com>
2022-09-22 10:16:26 +0900

    new release

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Wand wand = new Wand("wand","100");
+        Wand wand = new Wand("wand",5.5);
         Wizard w = new Wizard("wizard", 100,100,wand);
         Hero h = new Hero("minato",100);
         Matango m = new Matango('f',100);
